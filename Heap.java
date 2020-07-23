5e58c789faaedbdd0715524e1236e188f7c48d4b
swornimbarahi <swornimbarahi@gmail.com>
2020-07-22 20:39:30 -0400

    Added add functionality and bubble up functionality for heap

--- Heap.java
+++ Heap.java
@@ -1,12 +1,12 @@
 import java.io.*;
 
 class Heap {
-  private int[] heap;
+  private int[] storage;
   private int size;
   private int capacity;
 
   public Heap(int capacity) {
-    this.heap = new int[capacity];
+    this.storage = new int[capacity];
     this.size = 0;
     this.capacity = capacity;
   }
@@ -16,13 +16,34 @@ class Heap {
   }
 
   public int peek() {
-    return -1;
+    return this.size != 0 ? this.storage[0] : null;
   }
 
   public boolean add(int val) {
     if (this.size == this.capacity)
       return false;
+    this.storage[this.size] = val;
+    this.bubbleUp();
+    this.size++;
     return true;
+  }
+
+  private void bubbleUp() {
+    int idx = this.size;
+    while (this.getParentIdx(idx) != -1 && this.getParent(idx) > this.storage[idx]) {
+      this.swap(idx, this.getParentIdx(idx));
+      idx = this.getParentIdx(idx);
+    }
+  }
+
+  private void swap(int idx1, int idx2) {
+    int temp = this.storage[idx1];
+    this.storage[idx1] = this.storage[idx2];
+    this.storage[idx2] = temp;
+  }
+
+  public void bubbleDown() {
+    throw new UnsupportedOperationException("This method will be implemented in the next update.");
   }
 
   private int getParentIdx(int idx) {
@@ -34,18 +55,18 @@ class Heap {
   }
 
   private int getRightChildIdx(int idx) {
-    return idx * 2 < this.size ? idx * 2 + 1: -1;
+    return idx * 2 < this.size ? idx * 2 + 1 : -1;
   }
 
   private int getParent(int idx) {
-    return this.getParentIdx(idx) != -1 ? this.heap[this.getParentIdx(idx)] : null;
+    return this.getParentIdx(idx) != -1 ? this.storage[this.getParentIdx(idx)] : null;
   }
 
   private int getLeftChild(int idx) {
-    return this.getLeftChildIdx(idx) != -1 ? this.heap[this.getLeftChildIdx(idx)] : null;
+    return this.getLeftChildIdx(idx) != -1 ? this.storage[this.getLeftChildIdx(idx)] : null;
   }
 
   private int getRightChild(int idx) {
-    return this.getRightChildIdx(idx) != -1 ? this.heap[this.getRightChildIdx(idx)] : null;
+    return this.getRightChildIdx(idx) != -1 ? this.storage[this.getRightChildIdx(idx)] : null;
   }
 }
